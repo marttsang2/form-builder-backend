e8bbe0f6847ac95e402dcfa9cc6bb1deeb762789
Martin Tsang <tsangmartin1225@gmail.com>
2025-05-09 14:27:56 -0400

    feature: add /api global prefix

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,10 @@ async function bootstrap() {
   });
   app.use(json({ limit: '5mb' }));
   app.use(urlencoded({ extended: true, limit: '5mb' }));
+
+  // Set the global prefix for all routes if needed
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
